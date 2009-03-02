0d24414e4395ec584d3e7f64f93a433932adfd85
Shrimpy Wu <imxmwu@gmail.com>
2009-03-02 11:36:19 +0000

    upload basic project.

---
+++ wsclients/src/main/java/de/mail/MailClient.java
@@ -0,0 +1,26 @@
+package de.mail;
+
+import org.mail.mailschema.MailRequest;
+import org.mail.mailschema.MailResponse;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+public class MailClient {
+
+	private WebServiceTemplate webServiceTemplate;
+
+	public void setWebServiceTemplate(WebServiceTemplate webServiceTemplate) {
+		this.webServiceTemplate = webServiceTemplate;
+	}
+
+	public boolean send(String user, String password, String from, String to, String subject, String content) {
+		MailRequest request = new MailRequest();
+		request.setFrom(from);
+		request.setTo(to);
+		request.setUser(user);
+		request.setPassword(password);
+		request.setContent(content);
+		request.setSubject(subject);
+		return ((MailResponse)webServiceTemplate.marshalSendAndReceive(request)).isResult();
+	}
+
+}
