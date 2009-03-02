64c45d59ccac73ab32b2f35d6ccfed71cb966e4c
Shrimpy Wu <imxmwu@gmail.com>
2009-03-02 11:54:49 +0000

    Fixed uncompile issue.

--- wsclients/src/main/java/de/mail/MailClient.java
+++ wsclients/src/main/java/de/mail/MailClient.java
@@ -1,7 +1,5 @@
 package de.mail;
 
-import org.mail.mailschema.MailRequest;
-import org.mail.mailschema.MailResponse;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 public class MailClient {
@@ -13,14 +11,15 @@ public class MailClient {
 	}
 
 	public boolean send(String user, String password, String from, String to, String subject, String content) {
-		MailRequest request = new MailRequest();
-		request.setFrom(from);
-		request.setTo(to);
-		request.setUser(user);
-		request.setPassword(password);
-		request.setContent(content);
-		request.setSubject(subject);
-		return ((MailResponse)webServiceTemplate.marshalSendAndReceive(request)).isResult();
+//		MailRequest request = new MailRequest();
+//		request.setFrom(from);
+//		request.setTo(to);
+//		request.setUser(user);
+//		request.setPassword(password);
+//		request.setContent(content);
+//		request.setSubject(subject);
+//		return ((MailResponse)webServiceTemplate.marshalSendAndReceive(request)).isResult();
+        return true;
 	}
 
 }
