180bb5a274f72fb1da0150163e33f44fa71cb781
Shrimpy Wu <imxmwu@gmail.com>
2009-03-03 13:00:41 +0000

    Fixed formatting.

--- wsclients/src/main/java/edu/unsw/evaluation/Main.java
+++ wsclients/src/main/java/edu/unsw/evaluation/Main.java
@@ -28,34 +28,27 @@ public class Main {
 //            "</SayGoodDay>" +
 //            "</soap:Body>" +
 //            "</soap:Envelope>";
-    
     private static final String MESSAGE =
             "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">" +
             "<soap12:Body>" +
             "<SayGoodDay xmlns=\"http://azureva.org/\">" +
-            "<yourNamePlz>Clayton</yourNamePlz>"+
+            "<yourNamePlz>Clayton</yourNamePlz>" +
             "</SayGoodDay>" +
-            "</soap12:Body>"+
+            "</soap12:Body>" +
             "</soap12:Envelope>";
-    
+
     public static void main(String[] args) {
         try {
-//        Test test = (Test) context.getBean("testPojo");
-//        System.out.println("===>>>>>> starting... " + test.goodDay("Clay"));
             WebServiceTemplate webServiceTemplate = (WebServiceTemplate) context.getBean("webServiceTemplate");
             SaajSoapMessageFactory messageFactory = (SaajSoapMessageFactory) context.getBean("messageFactory");
 
             WebServiceMessage message = messageFactory.createWebServiceMessage(new ByteArrayInputStream(MESSAGE.getBytes()));
             message.writeTo(System.out);
-            System.out.println();
+            System.out.println("\n");
 
             webServiceTemplate.sendSourceAndReceiveToResult(message.getPayloadSource(), message.getPayloadResult());
             message.writeTo(System.out);
             System.out.println();
-
-
-//            DOMParser parser = new DOMParser();
-//            parser.setDocumentSource(message.getPayloadResult().);
 
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
