cb7b2c627610592bf8ad07c74afeb2b21a0bc1f7
Shrimpy Wu <imxmwu@gmail.com>
2009-03-24 10:40:43 +0000

    Enhanced CRUDByNumberTest.

--- wsclients/src/test/java/org/unsw/eva/wsclient/azure/CRUDByNumberTest.java
+++ wsclients/src/test/java/org/unsw/eva/wsclient/azure/CRUDByNumberTest.java
@@ -6,6 +6,8 @@ import org.cloudcomputingevaluation.ICloudComputingEvaluationReadDataByNumberClo
 import org.cloudcomputingevaluation.ICloudComputingEvaluationUpdateDataByNumberCloudComputatonEvaluationExceptionFaultMessage;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.unsw.eva.wsclient.AbstractTest;
 
 /**
@@ -14,6 +16,7 @@ import org.unsw.eva.wsclient.AbstractTest;
  */
 public class CRUDByNumberTest extends AbstractTest {
 
+    private static final Logger log = LoggerFactory.getLogger(CRUDByNumberTest.class);
     private static final int NUMBER_OF_DATA = 100;
 
     @Before
@@ -29,8 +32,8 @@ public class CRUDByNumberTest extends AbstractTest {
             ICloudComputingEvaluationDeleteDataByNumberCloudComputatonEvaluationExceptionFaultMessage {
 
         getAzureServiceEndpoint().createDataByNumber(NUMBER_OF_DATA);
-//        getAzureServiceEndpoint().updateDataByNumber(NUMBER_OF_DATA);
+        getAzureServiceEndpoint().updateDataByNumber(NUMBER_OF_DATA);
         getAzureServiceEndpoint().readDataByNumber(NUMBER_OF_DATA);
-//        getAzureServiceEndpoint().deleteDataByNumber(NUMBER_OF_DATA);
+        getAzureServiceEndpoint().deleteDataByNumber(NUMBER_OF_DATA);
     }
 }
