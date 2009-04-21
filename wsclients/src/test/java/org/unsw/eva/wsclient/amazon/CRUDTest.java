278c6fb1346c0927d807a188a38569ab18107641
Veyron Fei <veyronfei@gmail.com>
2009-04-21 13:34:57 +0000

    update test

--- wsclients/src/test/java/org/unsw/eva/wsclient/amazon/CRUDTest.java
+++ wsclients/src/test/java/org/unsw/eva/wsclient/amazon/CRUDTest.java
@@ -1,14 +1,13 @@
 package org.unsw.eva.wsclient.amazon;
 
-import org.cloudcomputingevaluation.ICloudComputingEvaluationCreateCloudComputatonEvaluationExceptionFaultMessage;
-import org.cloudcomputingevaluation.ICloudComputingEvaluationDeleteCloudComputatonEvaluationExceptionFaultMessage;
-import org.cloudcomputingevaluation.ICloudComputingEvaluationReadCloudComputatonEvaluationExceptionFaultMessage;
-import org.cloudcomputingevaluation.ICloudComputingEvaluationUpdateCloudComputatonEvaluationExceptionFaultMessage;
 import org.cloudcomputingevaluation.Result;
+import org.junit.After;
+import org.junit.Before;
 import org.unsw.eva.wsclient.AbstractTest;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 /**
  * @author fei
  */
@@ -16,12 +15,13 @@ public class CRUDTest extends AbstractTest {
 
     private static final String CONTENT = "This is the key";
 
+    @Before
+    public void setUp() throws Exception {
+        getAmazonEndpoint().cleanDefaultData(0, 0);
+    }
+
     @Test
-    public void testCRUD()
-            throws ICloudComputingEvaluationCreateCloudComputatonEvaluationExceptionFaultMessage,
-            ICloudComputingEvaluationReadCloudComputatonEvaluationExceptionFaultMessage,
-            ICloudComputingEvaluationUpdateCloudComputatonEvaluationExceptionFaultMessage,
-            ICloudComputingEvaluationDeleteCloudComputatonEvaluationExceptionFaultMessage {
+    public void testCRUD() throws Exception {
 
         String id;
         Result result = getAmazonEndpoint().create(CONTENT);
@@ -35,6 +35,10 @@ public class CRUDTest extends AbstractTest {
         assertTrue(id.equals(result.getId().getValue()));
 
         result = getAmazonEndpoint().delete(CONTENT + CONTENT);
+    }
 
+    @After
+    public void cleanUp() throws Exception {
+        getAmazonEndpoint().cleanDefaultData(0, 0);
     }
 }
