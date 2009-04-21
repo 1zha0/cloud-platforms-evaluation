5cb3e474eee5fa5e26415a7342056eaffc2e3ba3
Veyron Fei <veyronfei@gmail.com>
2009-04-21 13:27:34 +0000

    update counter test

--- wsclients/src/test/java/org/unsw/eva/wsclient/amazon/CounterTest.java
+++ wsclients/src/test/java/org/unsw/eva/wsclient/amazon/CounterTest.java
@@ -1,10 +1,9 @@
 package org.unsw.eva.wsclient.amazon;
 
-import org.cloudcomputingevaluation.ICloudComputingEvaluationGetCounterCloudComputatonEvaluationExceptionFaultMessage;
-import org.cloudcomputingevaluation.ICloudComputingEvaluationResetCounterCloudComputatonEvaluationExceptionFaultMessage;
 import org.unsw.eva.wsclient.AbstractTest;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 /**
  *
  * @author fei
@@ -12,10 +11,7 @@ import static org.junit.Assert.*;
 public class CounterTest extends AbstractTest {
 
     @Test
-    public void testReadAndResetCounter()
-            throws ICloudComputingEvaluationResetCounterCloudComputatonEvaluationExceptionFaultMessage,
-            ICloudComputingEvaluationGetCounterCloudComputatonEvaluationExceptionFaultMessage {
-
+    public void testReadAndResetCounter() throws Exception {
         getAmazonEndpoint().resetCounter();
         assertTrue(getAmazonEndpoint().getCounter() == 0);
     }
