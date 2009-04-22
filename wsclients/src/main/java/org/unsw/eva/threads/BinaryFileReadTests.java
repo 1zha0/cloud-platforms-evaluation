f5c60da084210b694f16bba932f32768721e933a
Veyron Fei <veyronfei@gmail.com>
2009-04-22 18:28:44 +0000

    Added binary file read test

---
+++ wsclients/src/main/java/org/unsw/eva/threads/BinaryFileReadTests.java
@@ -0,0 +1,47 @@
+package org.unsw.eva.threads;
+
+import org.cloudcomputingevaluation.ICloudComputingEvaluationBinaryFileReadCloudComputatonEvaluationExceptionFaultMessage;
+import org.cloudcomputingevaluation.Result;
+import org.unsw.eva.exceptions.ServerError;
+import org.unsw.eva.SOAPVersion;
+import org.unsw.eva.ServerType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.unsw.eva.strategy.AbstractStrageyTest;
+
+/**
+ * @author fei
+ */
+public class BinaryFileReadTests<T extends AbstractStrageyTest> extends EvaluationThread {
+
+    private static final Logger log = LoggerFactory.getLogger(BinaryFileReadTests.class);
+    private static int counter = 0;
+
+    public BinaryFileReadTests() {
+    }
+
+    public BinaryFileReadTests(String name, T strageyTest, ServerType serverType, int repeatNumberOfTime) {
+        super(name, strageyTest, SOAPVersion.SOAP_11, serverType, repeatNumberOfTime);
+    }
+
+    @Override
+    public Result doSOAP11Call() {
+        try {
+            counter++;
+            return getServiceEndpoint().binaryFileRead(String.valueOf(counter));
+        } catch (ICloudComputingEvaluationBinaryFileReadCloudComputatonEvaluationExceptionFaultMessage ex) {
+            throw new ServerError(ex.getFaultInfo().getReason().getValue());
+        }
+    }
+
+    @Override
+    public Result doSOAP12Call() {
+        return null;
+    }
+
+    @Override
+    public Boolean hasError() {
+        return getResult().getId() == null;
+    }
+}
